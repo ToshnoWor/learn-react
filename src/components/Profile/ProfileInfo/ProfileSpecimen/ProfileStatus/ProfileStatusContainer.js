543daca6cca23de9c333d6adca9c8bd62c3f0758
AndaRun <alekseikoval69@gmail.com>
2021-06-02 05:53:55 +0300

    hook, useState

--- src/components/Profile/ProfileInfo/ProfileSpecimen/ProfileStatus/ProfileStatusContainer.js
+++ src/components/Profile/ProfileInfo/ProfileSpecimen/ProfileStatus/ProfileStatusContainer.js
@@ -3,12 +3,13 @@ import {connect} from "react-redux";
 import {compose} from "redux";
 import ProfileStatus from "./ProfileStatus";
 import {saveStatus} from "../../../../../redux/profile-reducer";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 class ProfileStatusContainer extends React.Component{
 
     render(){
         return (
-            <ProfileStatus
+            <ProfileStatusWithHooks
                 status={this.props.status}
                 saveStatus={this.props.saveStatus}
                 isAuth={this.props.isAuth}
---
+++ src/components/Profile/ProfileInfo/ProfileSpecimen/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import s from '../ProfileSpecimen.module.css'
+
+
+const ProfileStatusWithHooks  = (props) => {
+
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+
+    const activateMode = () => {
+        setEditMode(true);
+    }
+    const deactivateMode = () => {
+        setEditMode(false);
+        props.saveStatus(props.auth ,status);
+    }
+    const onStatusChange = (e) => {
+        setStatus(e.target.value);
+    }
+
+    return (
+        < >
+            {!editMode &&
+                <div
+                    onDoubleClick={
+                        props.isAuth
+                            ? activateMode//this.activateEditMode
+                            : () => {alert("It's not your page!")}
+                    }>
+                        <span className={s.spanStatus}>
+                            {
+                                !status
+                                    ? "Not status"
+                                    :status
+                            }
+                        </span>
+                </div>
+            }
+            {editMode &&
+            <div>
+                <input className={s.inputStatus}
+                       autoFocus
+                       //onBlur={this.deactivateEditMode.bind(this)}=
+                       onChange={onStatusChange}
+                       value={status}
+                />
+                <button className={s.button}
+                        onClick={deactivateMode}
+                >Save</button>
+            </div>
+            }
+        </>
+    )
+}
+
+export default ProfileStatusWithHooks;
