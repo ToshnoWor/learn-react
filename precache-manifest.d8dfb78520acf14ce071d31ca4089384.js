65cc07555207b2ade83fb372c1613107a5de69a8
AndaRun <alekseikoval69@gmail.com>
2021-06-11 18:44:43 +0300

    Updates

---
+++ precache-manifest.d8dfb78520acf14ce071d31ca4089384.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae88af476eeb10df36ca63aead42607",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "657a61303b8fcc36aec8",
+    "url": "/learn-react/static/css/4.72c8ac73.chunk.css"
+  },
+  {
+    "revision": "06230bf60647cbda1e0f",
+    "url": "/learn-react/static/css/5.33e06e1a.chunk.css"
+  },
+  {
+    "revision": "eb91008a0b289d228391",
+    "url": "/learn-react/static/css/6.c50be086.chunk.css"
+  },
+  {
+    "revision": "e03f79becd693b6f7d30",
+    "url": "/learn-react/static/css/7.c6979aa7.chunk.css"
+  },
+  {
+    "revision": "b63d961185ca55b78dab",
+    "url": "/learn-react/static/css/8.560a6beb.chunk.css"
+  },
+  {
+    "revision": "462a6bf0f85d75b84e11",
+    "url": "/learn-react/static/css/main.2c8b9d7e.chunk.css"
+  },
+  {
+    "revision": "e30e9549eb393a044273",
+    "url": "/learn-react/static/js/0.ffbf65f7.chunk.js"
+  },
+  {
+    "revision": "074ac457df6c480d72a6",
+    "url": "/learn-react/static/js/3.0d7e7f50.chunk.js"
+  },
+  {
+    "revision": "657a61303b8fcc36aec8",
+    "url": "/learn-react/static/js/4.4af0b748.chunk.js"
+  },
+  {
+    "revision": "06230bf60647cbda1e0f",
+    "url": "/learn-react/static/js/5.ebe2ea5a.chunk.js"
+  },
+  {
+    "revision": "eb91008a0b289d228391",
+    "url": "/learn-react/static/js/6.2f4bd29a.chunk.js"
+  },
+  {
+    "revision": "e03f79becd693b6f7d30",
+    "url": "/learn-react/static/js/7.8b59eeef.chunk.js"
+  },
+  {
+    "revision": "b63d961185ca55b78dab",
+    "url": "/learn-react/static/js/8.ca3beb61.chunk.js"
+  },
+  {
+    "revision": "462a6bf0f85d75b84e11",
+    "url": "/learn-react/static/js/main.f025ef87.chunk.js"
+  },
+  {
+    "revision": "54c50ed1ffd56f49a27a",
+    "url": "/learn-react/static/js/runtime~main.665d7ec7.js"
+  },
+  {
+    "revision": "dde067c188a190c3c59f6e097055a5f0",
+    "url": "/learn-react/static/media/PreloaderNoBack.dde067c1.svg"
+  },
+  {
+    "revision": "9e883cf310c698ca9913e18a6b78660c",
+    "url": "/learn-react/static/media/defaultUserMan.9e883cf3.png"
+  }
+]);
